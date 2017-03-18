d123105ec25984dc9b8f72a104e733eef5776f1e
usuarioJapa <willian.hsimabukuro@gmail.com>
2017-03-17 23:45:50 -0300

    init

---
+++ js/index.service.js
@@ -0,0 +1,22 @@
+angular 
+    .module('app') 
+    .factory('Produto', function() {
+    
+    var Produto = {
+        salvar: salvar,
+        getProdutos: getProdutos,
+        produtos: []
+    };
+    return Produto;
+    
+    function salvar(prod) {
+        Produto.produtos.push(prod);
+        console.log(JSON.stringify(prod, null, 3)+" --Inserido--> "+JSON.stringify(Produto.produtos, null, 3)+"===============");
+    }
+    
+    function getProdutos(x) {
+        console.log("LOG LISTA ====== "+JSON.stringify(Produto.produtos, null, 3)+" ===============");
+        return Produto.produtos;
+    }
+    
+});
